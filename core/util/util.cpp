02c0ea46800b77241b2eff863530b01fc2c3ec79
Ivan <bokai.box@gmail.com>
2024-12-06 08:45:23 +0300

    refactor: minor changes

--- core/util/util.cpp
+++ core/util/util.cpp
@@ -1,10 +1,8 @@
 #include "util.h"
 namespace util {
-size_t word_size(const char* word){
+size_t word_size(const char* word) {
     size_t word_size = 0;
-    for(size_t i = 0; word[i] != '\0'; i++){
-        word_size++;
-    }
+    for (word_size; word[word_size] != '\0'; word_size++);
     return word_size;
 }
 }  // namespace util
